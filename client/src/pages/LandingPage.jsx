3eab78944d0c2d01d0162f8359b4b9424b7b145f
Paras1209 <kanojiaparas582@gmail.com>
2026-06-07 13:47:47 +0530

    Completed the steps section of landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Steps from '../components/Steps'
 import Hero from '../components/Hero'
 
 const LandingPage = () => {
   return (
     <div>
       <Hero/>
+      <Steps/>
     </div>
   )
 }
