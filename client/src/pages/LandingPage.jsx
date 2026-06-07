9f52287f33bcae7665cda0635ef57512a8416a9b
Paras1209 <kanojiaparas582@gmail.com>
2026-06-07 14:20:15 +0530

    created background slider image section for landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Steps from '../components/Steps'
 import Hero from '../components/Hero'
+import BgSlider from '../components/BgSlider'
 
 const LandingPage = () => {
   return (
     <div>
       <Hero/>
       <Steps/>
+      <BgSlider/>
     </div>
   )
 }
