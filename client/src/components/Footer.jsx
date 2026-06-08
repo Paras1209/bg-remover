40db4c1bae23293aba0ad9d0f93c5dd4dacc9f7c
Paras1209 <kanojiaparas582@gmail.com>
2026-06-08 23:00:28 +0530

    Completed results page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+// import React from "react";
 import { assets } from "../assets/assets";
 const Footer = () => {
   return (
     <div className="flex items-center justify-between gap-4 px-4 lg:px-44 py-3">
       <img width={150} src={assets.logo} alt="" />
-      <p className="flex-1 border-l-1 border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden">Copyright © 2026 Paras Dev. All rights reserved.</p>
+      <p className="flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden">Copyright © 2026 Paras Dev. All rights reserved.</p>
 
       {/* socials */}
       <div className="flex gap-1">
